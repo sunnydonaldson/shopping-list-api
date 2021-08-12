a766525ead787e58d75c5ce8389f5fefec1f339c
Sunny Donaldson <sunnydonaldson1@gmail.com>
2021-08-12 10:11:02 +0100

    adding Google authentication

--- app.js
+++ app.js
@@ -4,61 +4,120 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
+
+const app = express();
+
+// specifies a public directory for things like Stylesheets
+app.use(express.static('public')); 
+app.use(express.json())
+
+
+// sets up a session using the secret key retrieved from the environment variables
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//enable cross origin resource sharing to enable the api to be accessed from other origins (domains)
+app.use(cors({credentials: true, origin: 'http://localhost:3000',optionsSuccessStatus:200}))
+
+//connecting to the MongoDB atlas database, using the password stored as an environment variable
 mongoose.connect(`mongodb+srv://sunny-admin:${process.env.ADMIN_PASSWORD}@cluster0.2qq4y.mongodb.net/shopping-list?retryWrites=true&w=majority`,{useNewUrlParser:true});
+mongoose.set("useCreateIndex", true); 
 
 
-//----------------------------saving to mongodb-------------------------------//
-const listSchema = new mongoose.Schema({
+
+
+
+//defining a schema for the user documents so we know which fields to expect, and what their datatypes are
+const userSchema = new mongoose.Schema({
     user:String,
+    googleId:String,
     username:String,
     list:Array,
 
 })
-const List = mongoose.model("List",listSchema);
 
-const list = new List({
-    user:"123dkjf",
-    username:"test",
-    list:["cake","banana","unicorn","eggs","milk"]
-})
-
-// list.save();
+//plugs in the findOrCreate function to the userSchema, which provides an easy way for us to search for something,
+//and create it if it doesn't exist
+userSchema.plugin(findOrCreate)
 
 
-//------------------------reading from database-----------------------------//
+const User = mongoose.model("User",userSchema);
 
-List.find(function(err,lists){
-    if(err){
-        console.log(err);
-    }else{
-        console.log(lists);
-    }
+
+
+
+
+
+
+
+
+
+
+//sets the user ID as a cookie in the browser
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+  });
+
+//retrieves the user ID from the cookie in order to access the users data
+passport.deserializeUser(function(_id, done) {
+    User.findById(_id, function(err, user) {
+    done(err, user);
+    });
 });
 
+//sets up authentication with google
+passport.use(new GoogleStrategy({
+    //gets the clientID and clientSecret from the environment variables
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
 
-const app = express();
-app.use(cors())
+    //redirects to this route once complete
+    callbackURL: "http://localhost:9000/auth/google/callback",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    //searches the MongoDB database for a user who's googleId matches the profile ID.
+    //It is then retrieved if it exists, or created if it doesn't.
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
 
-// passport.use(new GoogleStrategy({
-//     clientID: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     callbackURL: "http://localhost:9000/auth/google/shopping"
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//        User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//          return done(err, user);
-//        });
-//   }
-// ));
+//responds to get request on "/auth/google" by using passport.authenticate, which redirects to the Google sign-in page.
+app.get("/auth/google",passport.authenticate('google', { scope: ["profile"] }));
 
 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: 'http://localhost:3000' }),
+  function(req, res) {
+    //redirects the authenticated user to the shopping list page 
+    res.redirect('http://localhost:3000/shopping');
+  });
 
+
+  app.get("/get-list",function(req,res){
+      //sends the 
+      res.send(req.user)
+  })
+
+
+//The root route isnt' actually needed, but I set it up to respond with a status message incase someone gets directed here by accident
 app.get("/",function(req,res){
-    console.log("requested");
     res.send({status:"good"});
 })
 
+//sets the server to listen on port 9000 for any requests
 app.listen(9000,function(){
     console.log("the server is up on port 9000");
 });
